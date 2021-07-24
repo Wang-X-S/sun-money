d4d65325b38928108f7b3c378bd002985dd97083
13jun <820373584@qq.com>
2021-07-24 13:47:28 +0800

    使用icon，引入svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money" />
+      </svg>
+      记账</router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#label" />
+      </svg>
+      标签</router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics" />
+      </svg>
+      统计</router-link>
   </div>
 </template>
 
 <script>
+  import money from '@/assets/icons/money.svg'
+  import label from '@/assets/icons/label.svg'
+  import statistics from '@/assets/icons/statistics.svg'
+
   export default {
     name: "Nav"
   }
