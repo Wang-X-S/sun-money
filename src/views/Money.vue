88e8fcf35e51f4eae62844f6f61d089edfe45bfe
13jun <820373584@qq.com>
2021-07-24 11:57:25 +0800

    使用flex在 Money.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>money
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>money</p>>
+    </div>
   <Nav/>
   </div>
 </template>
@@ -13,5 +16,16 @@
 </script>
 
 <style scoped>
+.nav-wrapper{
+  border:1px solid green;
+  display:flex;
+  flex-direction: column;
+  height:100vh;
+}
+.content{
+  border:1px solid blue;
+  flex-grow:1;
+  overflow: auto;
 
+}
 </style>
