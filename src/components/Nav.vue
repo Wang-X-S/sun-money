5fbaa82dcdfb0f13e55a8867daff855f6473cd80
13jun <820373584@qq.com>
2021-07-24 16:05:11 +0800

    添加 icon Nav样式，"__WebpackModuleApi":"writable"报错

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,37 +1,47 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <svg>
-        <use xlink:href="#money" />
-      </svg>
+  <nav class="nav">
+    <router-link to="/money" class="item">
+      <Icon name="money"/>
       记账</router-link>
-    |
-    <router-link to="/labels">
-      <svg>
-        <use xlink:href="#label" />
-      </svg>
+
+    <router-link to="/labels" class="item">
+      <Icon name="label"/>
       标签</router-link>
-    |
-    <router-link to="/statistics">
-      <svg>
-        <use xlink:href="#statistics" />
-      </svg>
+
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics" />
       统计</router-link>
-  </div>
+  </nav>
 </template>
 
-<script>
-  import money from '@/assets/icons/money.svg'
-  import label from '@/assets/icons/label.svg'
-  import statistics from '@/assets/icons/statistics.svg'
-
+<script lang="ts">
+  let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
   export default {
-    name: "Nav"
+    name: "Nav",
+    components:{}
   }
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border:1px solid red
+  nav {
+    display:flex;
+    box-shadow:0 0 3px rgba(0,0,0,0.25);
+    flex-direction: row;
+    > .item {
+      padding:2px 0;
+      width:33.3333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon{
+        width:32px;
+        height:32px;
   }
+
+
+    }
+  }
+
 </style>
