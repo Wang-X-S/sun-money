fda273c2b486c88fe2c299346a62fa2a838d122e
13jun <820373584@qq.com>
2021-07-26 12:38:11 +0800

    把Money分成四个组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="tags">
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Tags"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tags {
+    font-size: 14px;
+    padding: 16px;
+    flex-grow: 1;
+
+    > .current {
+      display: flex;
+      overflow: auto;
+      height: 64px;
+
+      > li {
+        background: #d9d9d9;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 12px;
+        padding: 0 16px;
+        margin-right: 12px;
+      }
+    }
+
+    > .current::-webkit-scrollbar {
+      width: 0 !important
+    }
+
+    > .new {
+
+      padding-top: 16px;
+      position: relative;
+
+      > button {
+        position: absolute;
+        background: transparent;
+        border: none;
+        color: #999;
+        padding: 0 3px;
+      }
+    }
+  }
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Types"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    font-size: 24px;
+    text-align: center;
+
+    > li {
+      height: 64px;
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        background: #333;
+        width: 100%;
+        height: 4px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+      }
+    }
+  }
+</style>
