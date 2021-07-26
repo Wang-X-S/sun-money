00a1d9680881d7b2e5c8c31abfd10c239572c3b0
13jun <820373584@qq.com>
2021-07-26 13:14:03 +0800

    使用TS完成Type切换

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -7,23 +7,36 @@
   </div>
 </template>
 
-<script>
-  export default {
-    name: "Types",
-    data(){
-      return {
-        type:'-'
-      }
-    },
-    methods:{
-      selectType(type){
-        if(type !== '-' && type !=='+'){
-          throw new Error('type is unknown')
-        }
-        this.type = type
-      }
+<script lang="ts">
+  import Vue from 'vue'
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Types extends Vue{
+    type='-';
+    selectType(type:string){
+      if(type !== '-' && type !=='+'){
+                throw new Error('type is unknown')
+              }
+              this.type = type
     }
   }
+  // export default {
+  //   name: "Types",
+  //   data(){
+  //     return {
+  //       type:'-'
+  //     }
+  //   },
+  //   methods:{
+  //     selectType(type){
+  //       if(type !== '-' && type !=='+'){
+  //         throw new Error('type is unknown')
+  //       }
+  //       this.type = type
+  //     }
+  //   }
+  // }
 </script>
 
 <style lang="scss" scoped>
