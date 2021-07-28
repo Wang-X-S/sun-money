749ae23373f1cec0149dd8edf8cc38009633d3f0
13jun <820373584@qq.com>
2021-07-28 22:05:28 +0800

    完成EditLabel的CSS

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,44 +1,105 @@
 <template>
-  <Layout>
-    <div>
-      <Icon name="left"/>
-      <span>编辑标签</span>
+
+  <div class="wrapper">
+    <header>
+    <div class="navBar">
+      <Icon name="left" class="left"/>
+      <span class="title">编辑标签</span>
+      <span class="right"></span>
     </div>
-    <InputBox field-name="标签名" placeholder="在这里输入标签名" @update:value="onUpdateInput" />
-    <InputBox field-name="日期" placeholder="在这里输入备注" @update:value="onUpdateInput"/>
-    <InputBox field-name="备注" placeholder="" @update:value="onUpdateInput"/>
-    <InputBox field-name="金额" placeholder="" @update:value="onUpdateInput"/>
-  </Layout>
+    </header>
+    <main>
+    <div class="form-wrapper">
+      <InputBox field-name="标签名" placeholder="在这里输入标签名" @update:value="onUpdateInput"/>
+      <InputBox field-name="日期" placeholder="在这里输入备注" @update:value="onUpdateInput"/>
+      <InputBox field-name="备注" placeholder="" @update:value="onUpdateInput"/>
+      <InputBox field-name="金额" placeholder="" @update:value="onUpdateInput"/>
+    </div>
+    </main>
+    <footer>
+    <div class="button-wrapper">
+      <button>编辑完成</button>
+      <button class="delete">删除</button>
+    </div>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import tagListModel from "@/model/tagListModel"
+  import tagListModel from '@/model/tagListModel';
   import Notes from '@/components/Money/Notes.vue';
   import InputBox from '@/components/InputBox.vue';
+
   @Component({
     components: {InputBox, Notes}
   })
   export default class EditLabel extends Vue {
-    onUpdateInput(value:string){
-      console.log(value)
+    onUpdateInput(value: string) {
+      console.log(value);
     }
+
     created() {
       const id = this.$route.params.id;
-      tagListModel.fetch()
-      const tags = tagListModel.data
-      const tag = tags.filter(t =>t.id === id)[0]
-      console.log(tag)
-       if(tag){
-         console.log(tag)
-      }else{
-        this.$router.replace('/404')
+      tagListModel.fetch();
+      const tags = tagListModel.data;
+      const tag = tags.filter(t => t.id === id)[0];
+      if (tag) {
+        console.log(tag);
+      } else {
+        this.$router.replace('/404');
       }
     }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import '~@/assets/style/helper.scss';
+
+  .navBar {
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 16px;
+    align-items: center;
+    font-size: 18px;
+    background: $color-fontBlue;
+    color: #ffffff;
+    font-weight: 500;
+
+    > .left {
+      width: 24px;
+      height: 24px;
+    }
+
+    > .right {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  .wrapper{
+    position: relative;
+
+    height:100vh;
+  }
+  .button-wrapper{
+    position: absolute;
+    bottom:0;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction: row;
+
+    button{
+      border:none;
+      box-shadow:0 0 3px $color-shadow;
+      width:50vw;
+      height:10vh;
+      background: #ffffff; ;
+    }
+    .delete{
+      color:red;
+    }
+  }
 
 </style>
