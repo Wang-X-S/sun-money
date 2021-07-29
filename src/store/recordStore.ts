60f1a94d52453dda1e477defd89531b9d2e0787e
13jun <820373584@qq.com>
2021-07-29 21:41:04 +0800

    模块化的 store

---
+++ src/store/recordStore.ts
@@ -0,0 +1,7 @@
+import recordListModel from '@/model/recordListModel';
+
+export default {
+  recordList:recordListModel.fetch(),
+  createRecord:(record:RecordItem)=>recordListModel.create(record),
+
+}
