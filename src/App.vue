215e052eb092c738e875963a821effad8847759f
13jun <820373584@qq.com>
2021-08-06 17:51:01 +0800

    更改打开时的样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
   </div>
 </template>
@@ -17,5 +17,8 @@ body{
   color:#333;
   font-size: 16px;
 }
-
+#app{
+  max-width:500px;
+  margin:0 auto ;
+}
 </style>
